0c76396dfc1e471fcc56b56d1882aa468b0395d9
Lewis98 <lewis.stokes98@gmail.com>
2023-02-16 11:30:41 +0000

    Added Navbar component and basic routing + /components/NavBar.js + Routing to /App.js for 2 basic pages + Global background image to App.js (Requires further styling)

---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { Link } from "react-router-dom";
+
+import { Menu } from 'antd';
+
+
+const MenuStyle = {
+  position: 'relative',
+  display: 'flex',
+  justifyContent: 'center'
+};
+
+
+
+export default class Navbar extends Component {
+  render() {
+    return (
+        <>
+            <Menu style={MenuStyle} theme="dark" mode="horizontal">
+              <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
+              <Menu.Item key="2"><Link to="/Portfolio">Portfolio</Link></Menu.Item>
+            </Menu> 
+            
+        </>
+    )
+  }
+}
