98191d2beac897d49677a825ec41605924f61121
Lewis98 <lewis.stokes98@gmail.com>
2023-02-16 12:08:02 +0000

    Added pgPortfolio page (Currently blank) + Class component in ./src/components/pgPortfolio.js

---
+++ src/components/pgPortfolio.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import { Card } from 'antd';
+
+const { Meta } = Card;
+
+export default class Portfolio extends Component {
+  render() {
+    return (
+      <>
+                
+      </>
+    )
+  }
+}
