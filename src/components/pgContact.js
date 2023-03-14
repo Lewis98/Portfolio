1c33a1120e0465833abae52b600491bce5193f83
Lewis98 <lewis.stokes98@gmail.com>
2023-03-14 20:20:42 +0000

    Contact page
    + Complete rework of contact form style
    
    Portfolio page
    
    + Added Python AI Covid-19 detection project to portfolio

--- src/components/pgContact.js
+++ src/components/pgContact.js
@@ -1,78 +1,98 @@
 import React, { Component } from 'react'
-import { Button, Checkbox, Form, Input } from 'antd';
 
+import { Form, Input, Button } from 'antd'
 const { TextArea } = Input;
 
-const onFinish = (values: any) => {
-  console.log('Success:', values);
-};
 
-const onFinishFailed = (errorInfo: any) => {
-  console.log('Failed:', errorInfo);
-};
-
-
+// - - - - - - Styling - - - - - - \\
 const containerStyle = {
-  textAlign: 'center',
-  justifyContent: 'center',
-  width: '100%',  
+    textAlign: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    marginTop: '5em',      
 }
 
 const formStyle = {
 
-  textAlign: 'center',
-  justifyContent: 'center',
+    backgroundColor: 'rgb(0,0,0,0.25)',
 
-  maxWidth: '600px',
-  margin: 'auto',  
+    textAlign: 'center',
+    justifyContent: 'center',
+    
+    width: '600px',
+    maxWidth: '80vw',
+    margin: 'auto',
+    border: 'solid 2px',
+    borderRadius: '25px',
+    padding: '1em',     
+}
+
+const titleStyle = {
+    color: 'white',
+    textAlign: 'center',
+    padding: '0.5em',
+    margin: '0'
 }
 
 const fItemStyle = {
-  width: '100%',
-  margin: 'auto', 
+    width: '100%',
+    margin: 'auto', 
+    marginTop: '3em',
 }
+// - - - - - Styling  End - - - - - \\
 
-export default class pgContact extends Component {
+
+// - - - Form Submission Logic - - - \\
+const onFinish = (values) => {
+    console.log('Form submitted with following data:');
+    console.table(values)
+};
+
+const onFinishFailed = (errorInfo) => {
+    console.log('Sorry, your message could not be sent, please see below info.');
+    console.table(errorInfo);
+    console.log(errorInfo);
+};
+// - - Form Submission Logic End - - \\
+
+
+export default class Contact extends Component {
   render() {
     return (
-      <div style={containerStyle}>
+        <div style={containerStyle}>
         <Form style={formStyle}
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
         >
-          <Form.Item style={fItemStyle}
-            name="message"
-            rules={[{ required: true, message: 'Please type a message' }]}
-          >
-            <TextArea
-              showCount
-              maxLength={100}
-              style={{ height: 120, resize: 'none', width: '100%' }}
-              placeholder="Message"
-            />
-          </Form.Item>
+            <legend><h1 style={titleStyle}>Contact Form</h1></legend>
+            <Form.Item style={fItemStyle}
+                name="message"
+                rules={[{ required: true, message: 'Please type a message' }]}
+            >
+                <TextArea
+                showCount
+                maxLength={100}
+                style={{ height: 120, resize: 'none', width: '100%' }}
+                placeholder="Message"
+                />
+            </Form.Item>
+          
+            <Form.Item style={fItemStyle}
+                name="email"
+                rules={[{ required: true, message: 'Please input a valid email address' }]}
+            >
+                <Input
+                    placeholder="Email"
+                />
+            </Form.Item>
 
-          <Form.Item
-            name="email"
-            rules={[{ required: true, message: 'Please input a valid email address' }]}
-          >
-            <Input
-              placeholder="Email"
-            />
-          </Form.Item>
-
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
+            <Form.Item style={fItemStyle}>
+                <Button type="primary" htmlType="submit">
+                    Send Email
+                </Button>
+            </Form.Item>
         </Form>
-      </div>
+        </div>
     )
   }
 }
