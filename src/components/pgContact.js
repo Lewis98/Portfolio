60f18324195ae936abf14cac9a9ce8af3191aaf8
lewis98 <lewis.stokes98@gmail.com>
2023-03-16 00:22:03 +0000

    Contact Form underlying functionality
    + EmailJS Package for sending contact form responses
    + Name field added on contact form
    + Example .env file with required environment variable names
    + Form cleared upon submission
    
    TODO
    > Inform user of success on form submit

--- src/components/pgContact.js
+++ src/components/pgContact.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
+import emailjs from '@emailjs/browser'
 
 import { Form, Input, Button } from 'antd'
 const { TextArea } = Input;
 
+
+/**
+ * TODO:
+ * Prompt user to inform upon success of email sent
+ */
 
 // - - - - - - Styling - - - - - - \\
 const containerStyle = {
@@ -41,58 +47,86 @@ const fItemStyle = {
 }
 // - - - - - Styling  End - - - - - \\
 
-
-// - - - Form Submission Logic - - - \\
-const onFinish = (values) => {
-    console.log('Form submitted with following data:');
-    console.table(values)
-};
-
-const onFinishFailed = (errorInfo) => {
-    console.log('Sorry, your message could not be sent, please see below info.');
-    console.table(errorInfo);
-    console.log(errorInfo);
-};
-// - - Form Submission Logic End - - \\
-
-
 export default class Contact extends Component {
-  render() {
-    return (
-        <div style={containerStyle}>
-        <Form style={formStyle}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-        >
-            <legend><h1 style={titleStyle}>Contact Form</h1></legend>
-            <Form.Item style={fItemStyle}
-                name="message"
-                rules={[{ required: true, message: 'Please type a message' }]}
-            >
-                <TextArea
-                showCount
-                maxLength={100}
-                style={{ height: 120, resize: 'none', width: '100%' }}
-                placeholder="Message"
-                />
-            </Form.Item>
-          
-            <Form.Item style={fItemStyle}
-                name="email"
-                rules={[{ required: true, message: 'Please input a valid email address' }]}
-            >
-                <Input
-                    placeholder="Email"
-                />
-            </Form.Item>
 
-            <Form.Item style={fItemStyle}>
-                <Button type="primary" htmlType="submit">
-                    Send Email
-                </Button>
-            </Form.Item>
-        </Form>
-        </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+    }
+
+    formRef = React.createRef();
+
+    // - - - Form Submission Logic - - - \\
+    onFinish = (values) => {
+        console.log('Form submitted with following data:');
+        console.table(values)
+
+        emailjs.send(
+            process.env.REACT_APP_EJS_SERVICE_ID, 
+            process.env.REACT_APP_EJS_TEMPLATE_ID, 
+            values, 
+            process.env.REACT_APP_EJS_PUBLIC_KEY
+        ).then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+
+        this.formRef.current.resetFields();
+    };
+
+    onFinishFailed = (errorInfo) => {
+        console.log('Sorry, your message could not be sent, please see below info.');
+        console.table(errorInfo);
+        console.log(errorInfo);
+    };
+
+    render() {
+        return (
+            <div style={containerStyle}>
+            <Form style={formStyle}
+                onFinish={this.onFinish}
+                onFinishFailed={this.onFinishFailed}
+                ref={this.formRef}
+            >
+                <legend><h1 style={titleStyle}>Contact Form</h1></legend>
+
+                <Form.Item style={fItemStyle}
+                    name="user_name"
+                    rules={[{ required: false }]}
+                >
+                    <Input
+                        placeholder="Name"
+                    />
+                </Form.Item>
+
+                <Form.Item style={fItemStyle}
+                    name="user_email"
+                    rules={[{ required: true, message: 'Please input a valid email address' }]}
+                >
+                    <Input
+                        placeholder="Email"
+                    />
+                </Form.Item>
+
+                <Form.Item style={fItemStyle}
+                    name="user_message"
+                    rules={[{ required: true, message: 'Please type a message' }]}
+                >
+                    <TextArea
+                    showCount
+                    maxLength={100}
+                    style={{ height: 120, resize: 'none', width: '100%' }}
+                    placeholder="Message"
+                    />
+                </Form.Item>
+
+                <Form.Item style={fItemStyle}>
+                    <Button type="primary" htmlType="submit">
+                        Send Message
+                    </Button>
+                </Form.Item>
+            </Form>
+            </div>
+        )
+    }
 }
